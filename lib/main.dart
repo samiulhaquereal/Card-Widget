18eaaec369b16fa2d887cf1223bc032821bf5a22
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-10-25 21:11:24 +0600

    1st

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+          elevation: 20,
+          color: Colors.redAccent,
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 8,),
+                Text('This is a flutter card',style: TextStyle(color: Colors.white),),
+                TextButton(onPressed: (){}, child: Text('Click',style: TextStyle(color: Colors.yellow),))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
